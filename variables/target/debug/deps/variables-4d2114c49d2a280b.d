44a5c3fc62477cadc8973f0c0dda5330ae6fd488
NacasNils <guslyrni@student.gu.se>
2025-09-05 16:12:19 +0200

    started chapter 3

---
+++ variables/target/debug/deps/variables-4d2114c49d2a280b.d
@@ -0,0 +1,5 @@
+C:\Users\46709\Desktop\CODE\Rust\variables\target\debug\deps\variables-4d2114c49d2a280b.d: src\main.rs
+
+C:\Users\46709\Desktop\CODE\Rust\variables\target\debug\deps\variables-4d2114c49d2a280b.exe: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\46709\Desktop\CODE\Rust\variables\target\debug\variables.exe: C:\Users\46709\Desktop\CODE\Rust\variables\src\main.rs
